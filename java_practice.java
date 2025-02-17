1af8fef426d4c9bb865c4b0f11fb5d9faad01b09
Ouchinitsu <ouchinitsu@gmail.com>
2025-02-17 13:43:42 +0900

    getter finished
    2/17 13:43 ou

--- java_practice.java
+++ java_practice.java
@@ -1,13 +1,17 @@
 public class java_practice {
-    public static void main(String[] args) {
-        int[] nums = new int[10];
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = i + 1;
-            System.out.println(nums[i]);
-        }
+    private String name;
 
-        String[][][] members = { { { "suzuki" }, { "165cm", "65kg" } }, { { "tanaka" }, { "170cm", "70kg" } } };
-        System.out.println(members[0][1][1]);
-
+    java_practice(String value1) {
+        name = value1;
     }
+
+    public void showName() {
+        System.out.println(this.name);
+    }
+
+    public static void main(String[] args) {
+        java_practice obj1 = new java_practice("ou");
+        obj1.showName();
+    }
+
 }
