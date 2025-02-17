c954ee05ef9399e0021d9c97c2b5c720966bb408
Ouchinitsu <ouchinitsu@gmail.com>
2025-02-17 13:08:13 +0900

    array finished
    2/17 13:08 ou

--- java_practice.java
+++ java_practice.java
@@ -1,3 +1,9 @@
 public class java_practice {
-
+    public static void main(String[] args) {
+        int[] nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i + 1;
+            System.out.println(nums[i]);
+        }
+    }
 }
